f7d877c20de4486112a34048d3f63abab2c050ab
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2022-11-14 16:08:42 -0300

    Introdução: Aplicando conceitos de métodos acessores para a classe carro.

---
+++ Introducao/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ProjetoIntroducao
+{
+    public class Carro
+    {
+        private string marca;
+        private string modelo;
+        private string tipoDeCombustivel;
+
+        public string GetMarca ()
+        {
+            return this.marca;
+        }
+
+        public void SetMarca (string marca)
+        {
+            this.marca = marca;
+        }
+
+        public string GetModelo ()
+        {
+            return this.modelo;
+        }
+
+        public void SetModelo (string modelo)
+        {
+            this.modelo = modelo;
+        }
+
+        public string GetTipoDeCombustivel ()
+        {
+            return this.tipoDeCombustivel;
+        }
+
+        public void SetTipoDeCombustivel (string tipoDeCombustivel)
+        {
+            this.tipoDeCombustivel = tipoDeCombustivel;
+        }
+        public void Ligar ()
+        {
+            Console.WriteLine("Ligado!");
+            
+        }
+        public void Buzinar ()
+        {
+            Console.WriteLine("Desligado");
+        }
+    }
+}
